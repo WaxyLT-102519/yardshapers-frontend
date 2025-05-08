fe8057d9319ed08c4b6f232953ace990034e713e
WaxyLT-102519 <lukinator2020@gmail.com>
2025-05-08 18:52:02 -0400

    added back the missing index.html file

---
+++ src/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <div>Logo</div>
+      <div>Our Services</div>
+      <div>Contact Us!</div>
+    </div>
+  )
+}
+
+export default Header
