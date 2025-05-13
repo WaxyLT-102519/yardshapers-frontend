ffe20e78786e7eeb500944f74a732f32fc336a04
WaxyLT-102519 <lukinator2020@gmail.com>
2025-05-13 18:07:36 -0400

    styled the header component

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,21 +1,49 @@
-import { useNavigate } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 1em;
+  padding-right: 1em;
+  background-color: #22f;
+  color: white;
+`
+
+const Logo = styled.h1`
+  cursor: pointer;
+`
+
+const LinkContainer = styled(Container)`
+  width: 15%;
+`
+
+const LinkWrapper = styled(Link)`
+  color: white;
+  font-weight: bolder;
+  text-decoration: none;
+  transition: 500ms;
+  &:hover {
+    color: black;
+    transition: 200ms;
+  }
+`
 
 const Header = () => {
   const nav = useNavigate();
 
   return (
-    <div>
+    <Container>
       <div>
-        <h1 onClick={() => nav('/')}>Yardshapers</h1>
+        <Logo onClick={() => nav('/')}>Yardshapers</Logo>
       </div>
-      <div>
-        <Link to='/services'>Our Services</Link>
-      </div>
-      <div>
-        <Link to='/contact-us'>Contact Us!</Link>
-      </div>
-    </div>
+      <LinkContainer>
+        <LinkWrapper to='/services'>Our Services</LinkWrapper>
+        <LinkWrapper to='/contact-us'>Contact Us!</LinkWrapper>
+      </LinkContainer>
+    </Container>
   )
 }
 
