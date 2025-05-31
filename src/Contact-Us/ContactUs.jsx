cbedf5f4df4b646e2dfef9a97f90c7f989843c39
WaxyLT-102519 <lukinator2020@gmail.com>
2025-05-30 20:24:54 -0400

    added labels to the form fields

--- src/Contact-Us/ContactUs.jsx
+++ src/Contact-Us/ContactUs.jsx
@@ -1,4 +1,26 @@
 import { useState } from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`
+
+const Card = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 75%;
+`
+
+const JobRequestForm = styled.form`
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: center;
+  align-items: center;
+`
 
 const ContactUs = () => {
 
@@ -14,6 +36,7 @@ const ContactUs = () => {
 
   const submit = (e) => {
     e.preventDefault()
+    console.log(jobRequest)
   }
 
   const updateForm = (e) => {
@@ -21,45 +44,52 @@ const ContactUs = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={submit}>
-        <input 
-          id="first-name" 
-          type="text" 
-          name="firstName"
-          value={jobRequest.firstName} 
-          onChange={updateForm} 
-        />
-        <input 
-          id="last-name" 
-          type="text" 
-          name="lastName"
-          value={jobRequest.lastName} 
-          onChange={updateForm} 
-        />
-        <input 
-          id="phone-number" 
-          type="tel" 
-          name="phoneNumber"
-          value={jobRequest.phoneNumber} 
-          onChange={updateForm} 
-        />
-        <input 
-          id="email" 
-          type="email" 
-          name="email"
-          value={jobRequest.email} 
-          onChange={updateForm} 
-        />
-        <textarea
-          id="desciption"
-          name="description"
-          value={jobRequest.description}
-          onChange={updateForm}
-        />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <Container>
+      <Card>
+        <JobRequestForm onSubmit={submit}>
+          <label htmlFor="first-name">First Name</label>
+          <input 
+            id="first-name" 
+            type="text" 
+            name="firstName"
+            value={jobRequest.firstName} 
+            onChange={updateForm} 
+          />
+          <label htmlFor="last-name">Last Name</label>
+          <input 
+            id="last-name" 
+            type="text" 
+            name="lastName"
+            value={jobRequest.lastName} 
+            onChange={updateForm} 
+          />
+          <label htmlFor="phone-number">Phone Number</label>
+          <input 
+            id="phone-number" 
+            type="tel" 
+            name="phoneNumber"
+            value={jobRequest.phoneNumber} 
+            onChange={updateForm} 
+          />
+          <label htmlFor="email">Email Address</label>
+          <input 
+            id="email" 
+            type="email" 
+            name="email"
+            value={jobRequest.email} 
+            onChange={updateForm} 
+          />
+          <label htmlFor="description">What would you like us to do for your lawn?</label>
+          <textarea
+            id="desciption"
+            name="description"
+            value={jobRequest.description}
+            onChange={updateForm}
+          />
+          <button type="submit">Submit</button>
+        </JobRequestForm>
+      </Card>
+    </Container>
   )
 }
 
