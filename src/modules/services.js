28b52aca767c2fb288d68b5ede1f020ff11eb140
WaxyLT-102519 <lukinator2020@gmail.com>
2026-08-29 20:32:11 -0400

    Adds placeholder descriptions for issue #22

--- src/modules/services.js
+++ src/modules/services.js
@@ -4,51 +4,61 @@ const services = [
   {
     id: 1,
     label: "Landscape Design",
+    description: "Design a new lawn from scratch.",
     src: exampleImg
   },
   {
     id: 2,
     label: "Irrigation",
+    description: "This is a placeholder!",
     src: exampleImg
   },
   {
     id: 3,
     label: "Retaining Walls",
+    description: "This is a placeholder!",
     src: exampleImg
   },
   {
     id: 4,
     label: "Paver Patios & Walkways",
+    description: "This is a placeholder!",
     src: exampleImg
   },
   {
     id: 5,
     label: "Lawn Installation",
+    description: "This is a placeholder!",
     src: exampleImg
   },
   {
     id: 6,
     label: "Plant & Tree Installation",
+    description: "This is a placeholder!",
     src: exampleImg
   },
   {
     id: 7,
     label: "Tree Work & Hedge Trimming",
+    description: "This is a placeholder!",
     src: exampleImg
   },
   {
     id: 8,
     label: "Spring & Fall Clean-Up",
+    description: "This is a placeholder!",
     src: exampleImg
   },
   {
     id: 9,
     label: "Bed Maintenance",
+    description: "This is a placeholder!",
     src: exampleImg
   },
   {
     id: 10,
     label: "Mulch",
+    description: "This is a placeholder!",
     src: exampleImg
   }
 ]
