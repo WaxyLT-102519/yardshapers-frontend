513ac87cab2ff01255922e961e87623a38aababe
WaxyLT-102519 <lukinator2020@gmail.com>
2026-09-12 12:33:10 -0400

    Added a main layout component to get rid of header and footer

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
 import Home from './pages/Home/Home'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import Header from './layouts/Header/Header'
+import Footer from './layouts/Footer/Footer'
 import Services from './pages/Services/Services'
 import ContactUs from './pages/Contact-Us/ContactUs'
 
